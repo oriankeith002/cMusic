98d16c3de0fce490a6e0f8ec35d5c6aef43cf2d8
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-09-04 12:10:19 +0300

    Setting up Left Side Menu

---
+++ src/Components/LeftMenu.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../Styles/LeftMenu.css"
+import { FaSpotify, FaEllipsisH } from "react-icons/fa"
+
+
+const LeftMenu = () => {
+    return (
+        <div className="leftMenu">
+            <div className="logoContainer">
+                <i><FaSpotify /></i>
+                <h2>CwMusic Inc</h2>
+                <i><FaEllipsisH /></i>
+            </div>
+            <div className="searchBox"></div>
+
+        </div>
+    )
+} 
+
+
+export {LeftMenu}
