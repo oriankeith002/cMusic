29747dff1c95533a0525a5e847fa8bdf6027f987
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-09-04 19:53:00 +0300

    Complete Spotify Banner setup

--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -1,10 +1,11 @@
 import React from "react";
 import '../Styles/MainContainer.css';
+import { Banner } from "./Banner";
 
 const MainContainer = () => {
     return (
         <div className="mainContainer">
-
+            <Banner />
         </div>
     );
 }
