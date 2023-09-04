683f8898f20bdc86cb68008a0a05c3f2652cec02
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-09-04 20:24:02 +0300

    Added Menu List to the Main Container

--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useEffect} from "react";
 import '../Styles/MainContainer.css';
 import { Banner } from "./Banner";
+import { FaUsers } from "react-icons/fa";
+
 
 const MainContainer = () => {
+
+    // ToDo: Add event listener using useEffect to add active css class 
+
+
     return (
         <div className="mainContainer">
             <Banner />
+            <div className="menuList">
+                <ul> 
+                    <li><a href="#">Popular</a></li>
+                    <li><a href="#">Albums</a></li>
+                    <li><a href="#">Songs</a></li>
+                    <li><a href="#">Fans</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+                <p><i><FaUsers /></i> 12.3M <span>Followers</span></p>
+            </div>
         </div>
     );
 }
