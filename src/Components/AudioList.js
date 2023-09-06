81bc8fc80842acbb333037ef256c273dc8115d72
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-09-06 14:07:29 +0300

    Looping through the songs

--- src/Components/AudioList.js
+++ src/Components/AudioList.js
@@ -1,43 +1,49 @@
 import React from "react";
 import { FaHeadphones, FaHeart, FaRegClock, FaRegHeart } from "react-icons/fa";
-
+import { Songs } from "./Songs";
 
 const AudioList = () => {
 
     return (
         <div className="audioList">
-            <h2 className="title">The list <span>12 songs</span></h2>
+            <h2 className="title">The list <span>{`${Songs.length} songs`}</span></h2>
 
             <div className="songsContainer">
-                <div className="songs">
-                    <div className="count">#01</div>
-                    <div className="song">
-                        <div className="imgBox">
-                            <img src="" alt="" />
-                        </div> 
 
-                        <div className="section">
-                            <p className="songName">
-                                song name
-                                <span className="spanArtist"> Artist Name </span>
-                            </p> 
-                            <div className="hits">
-                                <p className="hit">
-                                    <i><FaHeadphones /></i>
-                                    95,490,102
-                                </p>
-                                <p className="duration">
-                                    <i><FaRegClock /></i>
-                                    03:04
-                                </p>
-                                <div className="favourite">
-                                    <i><FaHeart /></i>
-                                    <i><FaRegHeart /></i>
+                {
+                    Songs && Songs.map((song) => (
+                        <div className="songs" key={song?.id}>
+                            <div className="count">#{song?.id}</div>
+                            <div className="song">
+                                <div className="imgBox">
+                                    <img src="" alt="" />
+                                </div> 
+
+                                <div className="section">
+                                    <p className="songName">
+                                        {song?.songName}
+                                        <span className="spanArtist">{song?.artist}</span>
+                                    </p> 
+                                    <div className="hits">
+                                        <p className="hit">
+                                            <i><FaHeadphones /></i>
+                                            95,490,102
+                                        </p>
+                                        <p className="duration">
+                                            <i><FaRegClock /></i>
+                                            03:04
+                                        </p>
+                                        <div className="favourite">
+                                            <i><FaHeart /></i>
+                                            <i><FaRegHeart /></i>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
+                    ))
+                }
+               
             </div>
         </div>
     );
